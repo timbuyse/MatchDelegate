2662fd93ffc9cd2e20ab28b04c0be14ddd79e0ec
timbuyse <buysesorgeloos@gmail.com>
2026-06-28 12:04:01 +0200

    fix: service worker cache versie verhoogd naar v26, MD_cropped en splash-assets toegevoegd

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'voetbal-v25';
-const ASSETS = ['./', './index.html', './manifest.json', './logo.png'];
+const CACHE = 'voetbal-v26';
+const ASSETS = ['./', './index.html', './manifest.json', './MD_cropped.png', './logo_no_background.png', './background_logo.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
